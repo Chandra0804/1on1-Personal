5efe77f2507d271641e95abbe483abfe4ea33e8e
Chandra0804 <chandrasaiteja0804@gmail.com>
2023-09-15 21:16:01 +0530

    debugged

--- client/src/pages/ProgramDetails/ProgramDetails.js
+++ client/src/pages/ProgramDetails/ProgramDetails.js
@@ -1,5 +1,4 @@
 import React,{useState} from 'react'
-import { useLocation } from 'react-router-dom'; 
 // import { useParams } from 'react-router-dom';
 
 import Navbar from '../CommonComponents/components/navbar'
@@ -21,22 +20,17 @@ import ProgramDetailsRoadmap from './components/programDetailsRoadmap';
 // import axios from 'axios';
 // import RouteUrl from '../Routes/Routes';
 import data from './assets/json_data/program_details_info_in_json.json'
-import { program } from '@babel/types'
 
 
 export default function ProgramDetails() {
   // const { title } = useParams();
-  const location = useLocation(); // Use useLocation hook to access location object
-  const { title, courseDuration } = location.state || {}; // Destructure title and courseDuration from location.state
+  const props={
+    title:"Front-End Development",
+    courseDuration:"24 weeks"
+  }
+  // console.log(window.location.search.substring(1),' -> title')
 
-  // Ensure title and courseDuration have default values if not provided
-  const props = {
-    title: title || 'Default Title',
-    courseDuration: courseDuration || 'Default Course Duration',
-  };
-
-  const [programDetails, setProgramDetails] = useState(data[props.title][0]);
-
+  const [programDetails,setProgramDetails] = useState(data[props.title][0]);
   // const [validate,setValidate] = useState(0);
   // console.log("Data[0]" );
   // console.log(data['0'])
@@ -50,7 +44,7 @@ export default function ProgramDetails() {
       // ?
       <div className='program-details-page'>
        <Navbar/> 
-      <ProgramDetailsHero title={props.title} name={props.title} courseDuration={props.courseDuration} programimg={programDetails.program_banner_images}/>
+      <ProgramDetailsHero title={props.title} name={props.title} courseDuration={props.courseDuration} placementbgcolor={programDetails.bgcolor} />
       <ProgramDetailsCourseHighlights/> 
       <ProgramDetailsSkills skillsArr={programDetails.skills_images}/>
       <CareerPaths isCourseDetails={false} careeroptions = {programDetails.career_options}/>
